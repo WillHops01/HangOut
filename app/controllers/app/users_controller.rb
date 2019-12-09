c9543d36fb74d5c45e02eaecf35fb53aae372224
App Academy Student <email@email.com>
2019-12-09 14:17:29 -0500

    bugfixed user creation

--- app/controllers/app/users_controller.rb
+++ app/controllers/app/users_controller.rb
@@ -1,2 +1,16 @@
 class App::UsersController < ApplicationController
+
+  def create
+    @user = User.new(new_user_params)
+    if @user.save
+      login!(@user)
+      render :show
+    else
+    end
+  end
+
+  private
+  def new_user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,14 @@ class User < ApplicationRecord
   validates :password_digest, :long, :lat, presence: true
   validates :password, length: {minimum: 6}, allow_nil: true
   attr_reader :password
-  after_initialize :ensure_session_token!
+  after_initialize :ensure_session_token
 
   def ensure_session_token
     self.session_token ||= self.reset_session_token!
   end
 
   def generate_session_token!
-    SecureRandom::urlsafe_base(16)
+    SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
