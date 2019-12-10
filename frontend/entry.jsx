4aa000f23374a0e5402c38ee5cfe250ad02439a7
Will Hops <willhops01@gmail.com>
2019-12-10 09:01:19 -0500

    entities and user reducer

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,7 +6,8 @@ import Greeting from "./components/greeting";
 import configureStore from "./store/store";
 
 //testing
-import { login, logout } from "./util/session_api_util";
+//import { login, logout } from "./util/session_api_util";
+import { login } from "./actions/session_actions";
 
 const Root = ({store}) => (
   <Provider store={store}>
@@ -19,7 +20,13 @@ const Root = ({store}) => (
 document.addEventListener("DOMContentLoaded", ()=>{
   const root = document.getElementById("root");
   const store = configureStore();
+  //TESTING
   window.login = login;
-  window.logout = logout;
+  //window.logout = logout;
+  window.store = store;
+
+  //END TESTING
   ReactDOM.render(<Root store={store} />, root);
 })
+
+//const user1 = {username: "Will", password: "hunter2"}
